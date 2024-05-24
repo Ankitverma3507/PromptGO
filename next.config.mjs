c8c41b4d7bcaa2d1d9d3a18ebed99d5d121153f3
ANKIT VERMA <ankitvermaksk1234@gmail.com>
2024-05-25 01:47:26 +0530

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,25 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  
+  api:{
+    bodyParser :true,
+  },
+
+experimental: {
+  appDir: true,
+  serverComponentsExternalPackages: ["mongoose"],
+},
+images: {
+  domains: ['lh3.googleusercontent.com'],
+},
+webpack(config) {
+  config.experiments = {
+    ...config.experiments,
+    topLevelAwait: true,
+  }
+  return config
+}
+}
+
+export default nextConfig;
