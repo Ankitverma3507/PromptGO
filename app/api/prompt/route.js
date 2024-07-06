14d5208a10642a5e20ec654409e52b667861fd69
ANKIT VERMA <ankitvermaksk1234@gmail.com>
2024-07-07 01:29:23 +0530

    feed fixed

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,14 +1,19 @@
 import Prompt from "@models/prompt";
+import User from "@models/user";
 import { connectToDB } from "@utils/database";
 
 export const GET = async (request) => {
-    try {
-        await connectToDB()
-
-        const prompts = await Prompt.find({})
-
-        return new Response(JSON.stringify(prompts), { status: 200 })
-    } catch (error) {
-        return new Response("Failed to fetch all prompts", { status: 500 })
-    }
-} 
+  try {
+    await connectToDB();
+    const prompts = await Prompt.find({});
+    const newprompts = prompts.map(async (prompt) => {
+      const creator = await User.findById(prompt.creator);
+      prompt.creator = creator;
+    });
+    await Promise.all(newprompts)
+    console.log(prompts);
+    return new Response(JSON.stringify(prompts), { status: 200 });
+  } catch (error) {
+    return new Response("Failed to fetch all prompts", { status: 500 });
+  }
+};
